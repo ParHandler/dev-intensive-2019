00ca88fd3a1881a0f05e024ee3242195d88a72f6
ParHandler <kunilovskye@yandex.ru>
2019-07-15 10:10:54 +0300

    debug module3 FAILED java.lang.AssertionError: expected:<(null, null)> but was:<(, null)> next>>> module7 FAILED org.junit.ComparisonFailure: expected:<mi[_mi_]mi> but was:<mi[ mi ]mi>

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -18,12 +18,14 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
             "Это не правильный ответ!" to status.color
         }
     }
+
     enum class Status(val color: Triple<Int, Int, Int>) {
         NORMAL(Triple(255, 255, 255)),
         WARNING(Triple(255, 120, 0)),
         DANGER(Triple(255, 60, 60)),
         CRITICAL(Triple(255, 0, 0))
     }
+
     enum class Question(val question: String, val answers: List<String>) {
         NAME("Как меня зовут?", listOf("бендер", "bender")),
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")),
