59e1c19c4e951fd03a0dad0bfb7f297e86fede1e
ParHandler <kunilovskye@yandex.ru>
2019-07-20 16:14:08 +0300

    task3 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -19,15 +19,25 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
      (достаточно сохранить Status, Question)
      */
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
-        return if(question.answers.contains(answer)) {
-            question = question.nextQuestion()
-            "Отлично - ты справился\n${question.question}" to status.color
-        }else{
-            status  = status.nextStatus()
-            "Это неправильный ответ\n${question.question}" to status.color
+        return when(question){
+            Question.IDLE -> question.question to status.color
+            else -> {
+                if (question.answers.contains(answer)) {
+                        question = question.nextQuestion()
+                        "Отлично - ты справился\n${question.question}" to status.color
+                } else {
+                    if (status == Status.CRITICAL) {
+                        status = Status.NORMAL
+                        question = Question.NAME
+                        "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+                    } else {
+                        status = status.nextStatus()
+                        "Это неправильный ответ\n${question.question}" to status.color
+                    }
+                }
+            }
         }
     }
-
     enum class Status(val color: Triple<Int, Int, Int>) {
         NORMAL(Triple(255, 255, 255)),
         WARNING(Triple(255, 120, 0)),
@@ -35,9 +45,9 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         CRITICAL(Triple(255, 0, 0));
 
         fun nextStatus(): Status {
-            return if(this.ordinal<values().lastIndex){
-                values()[this.ordinal+1]
-            }else{
+            return if (this.ordinal < values().lastIndex) {
+                values()[this.ordinal + 1]
+            } else {
                 values()[0]
             }
 
@@ -48,21 +58,22 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         NAME("Как меня зовут?", listOf("бендер", "bender")) {
             override fun nextQuestion(): Question = PROFESSION
         },
-        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")){
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
             override fun nextQuestion(): Question = MATERIAL
         },
-        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")){
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
             override fun nextQuestion(): Question = BDAY
         },
-        BDAY("Когда меня создали?", listOf("2993")){
+        BDAY("Когда меня создали?", listOf("2993")) {
             override fun nextQuestion(): Question = SERIAL
         },
-        SERIAL("Мой серийный номер?", listOf("2716057")){
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
             override fun nextQuestion(): Question = IDLE
         },
-        IDLE("На этом все, вопросов больше нет", listOf()){
+        IDLE("На этом все, вопросов больше нет", listOf()) {
             override fun nextQuestion(): Question = IDLE
         };
+
         abstract fun nextQuestion(): Question
     }
 }
