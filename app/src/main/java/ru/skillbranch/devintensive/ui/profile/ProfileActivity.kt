dfcd4524318817bb5a9c6800ef7418166d0f334f
ParHandler <kunilovskye@yandex.ru>
2019-07-28 01:29:23 +0300

    process RelativeLayout

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -16,107 +16,15 @@ import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.models.Bender
 import ru.skillbranch.devintensive.models.Bender.Question
 
-class ProfileActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var benderImage: ImageView
-    lateinit var textTxt: TextView
-    lateinit var messageEt: EditText
-    lateinit var sendBtn: ImageView
-
-    lateinit var benderObj: Bender
-
-    override fun onCreate(savedInstanceState: Bundle?){
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-
-        benderImage = iv_bender
-        textTxt = tv_text
-        messageEt = et_message
-        sendBtn = iv_send
-
-        val status = savedInstanceState?.getString("STATUS") ?: Bender.Status.NORMAL.name
-        val question = savedInstanceState?.getString("QUESTION") ?: Bender.Question.NAME.name
-        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
-
-        Log.d("M_MainActivity","onCreate $status $question")
-
-        val(r,g,b) = benderObj.status.color
-        benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
-
-        textTxt.text = benderObj.askQuestions()
-        sendBtn.setOnClickListener(this)
-        onClickDoneButton(messageEt)
-    }
-
-    private fun onClickDoneButton(editText: EditText) {
-        editText.setOnEditorActionListener { _: TextView?, i: Int, _: KeyEvent? ->
-            if (i == EditorInfo.IME_ACTION_DONE) sendBtn.callOnClick()
-            false
-        }
-
-    }
-
-    private fun showValidationAnswer () {
-        val validationAnswer = when(benderObj.question) {
-            Question.NAME -> "Имя должно начинаться с заглавной буквы"
-            Question.PROFESSION -> "Профессия должна начинаться со строчной буквы"
-            Question.MATERIAL -> "Материал не должен содержать цифр"
-            Question.BDAY -> "Год моего рождения должен содержать только цифры"
-            Question.SERIAL -> "Серийный номер содержит только цифры, и их 7"
-            Question.IDLE -> "На этом все, вопросов больше нет"
-        }
-        textTxt.text = "${validationAnswer}\n${benderObj.question.question}"
-        messageEt.setText("")
-    }
-    private fun isValidAnswer() : Boolean = benderObj.question.validateAnswer(messageEt.text.toString()) ?: false
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("M_MainActivity","onRestart")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d("M_MainActivity","onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("M_MainActivity","onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("M_MainActivity","onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("M_MainActivity","onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("M_MainActivity","onDestroy")
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-
-        outState?.putString("STATUS", benderObj.status.name)
-        outState?.putString("QUESTION", benderObj.question.name)
-        Log.d("M_MainActivity", "onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")
     }
 
-    override fun onClick(v: View?) {
-        if (v?.id == R.id.iv_send) {
-            if (isValidAnswer()) {
-                val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase().trim())
-                messageEt.setText("")
-                val (r, g, b) = color
-                benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
-                textTxt.text = phrase
-            } else this.showValidationAnswer()
-        }
-    }
 }
 
