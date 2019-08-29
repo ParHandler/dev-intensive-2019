1dd369c7b3f0a0e00c118a5c1b77f72573f5d365
ParHandler <kunilovskye@yandex.ru>
2019-08-29 10:02:38 +0300

    activity_main edition
    Start 5 lesson hometask

--- app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
@@ -1,13 +1,32 @@
 package ru.skillbranch.devintensive.ui.main
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
+        initViews()
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+     }
+
+    private fun initViews() {
+
+    }
+
+    private fun initToolbar() {
+
     }
 }
