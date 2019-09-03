27fb5e30e908be5916627d5f024c011035b200d3
ParHandler <kunilovskye@yandex.ru>
2019-09-03 10:28:29 +0300

    ChatAdapter started

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/adapters/ChatAdapter.kt
@@ -0,0 +1,33 @@
+package ru.skillbranch.devintensive.ui.adapters
+
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.data.ChatItem
+
+class ChatAdapter : RecyclerView.Adapter<ChatAdapter.SingleViewHolder>() {
+    var items : List<ChatItem> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val convertView = inflater.inflate(R.layout.item_chat_single, parent, false)
+        Log.d("M_ChatAdapter", "onCreateViewHolder")
+        return SingleViewHolder(convertView)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: SingleViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class SingleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item:ChatItem){
+        }
+    }
+
+}
