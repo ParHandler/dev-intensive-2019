fa3902ad5ff202620b75712e2da13037d395d0df
ParHandler <kunilovskye@yandex.ru>
2019-10-15 10:25:19 +0300

    Добавляет Chips для группы пользователей

--- app/src/main/java/ru/skillbranch/devintensive/ui/group/GroupActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/group/GroupActivity.kt
@@ -2,14 +2,18 @@ package ru.skillbranch.devintensive.ui.group
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.children
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.activity_group.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.toolbar
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.data.UserItem
 import ru.skillbranch.devintensive.ui.adapters.UserAdapter
 import ru.skillbranch.devintensive.viewmodels.GroupViewModel
 
@@ -31,17 +35,44 @@ class GroupActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-        userAdapter = UserAdapter{viewModel.handleSelectedItem(it.id)}
+        userAdapter = UserAdapter { viewModel.handleSelectedItem(it.id) }
         val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
         with(rv_user_list) {
             adapter = userAdapter
             layoutManager = LinearLayoutManager(this@GroupActivity)
             addItemDecoration(divider)
         }
-     }
+    }
 
     private fun initViewModel() {
-        viewModel  = ViewModelProviders.of(this).get(GroupViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(GroupViewModel::class.java)
         viewModel.getUserData().observe(this, Observer { userAdapter.updateData(it) })
+        viewModel.getSelectedData().observe(this, Observer { updateChips(it) })
+    }
+
+    private fun addChipToGroup(user: UserItem) {
+        val chip = Chip(this).apply {
+            text = user.fullName
+            chipIcon = resources.getDrawable(R.drawable.avatar_default, theme)
+            isCloseIconVisible = true
+            tag = user.id
+            isClickable = true
+        }
+        chip.setOnCloseIconClickListener { viewModel.handleRemoveChip(it.tag.toString()) }
+        chip_group.addView(chip)
+    }
+
+    private fun updateChips(listUsers: List<UserItem>) {
+        chip_group.visibility = if (listUsers.isEmpty()) View.GONE else View.VISIBLE
+        val users = listUsers
+            .associate { user -> user.id to user }
+            .toMutableMap()
+        val views = chip_group.children.associate { view -> view.tag to view }
+
+        for ((k, v) in views) {
+            if (!users.containsKey(k)) chip_group.removeView(v)
+            else users.remove(k)
+        }
+        users.forEach { (_, v) -> addChipToGroup(v) }
     }
 }
