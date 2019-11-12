081dc4e84e4c455d71844048393c0756fd3dd8c7
ParHandler <kunilovskye@yandex.ru>
2019-11-12 10:18:38 +0300

    Добавляет ArchiveActivity

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/ArchiveActivity.kt
@@ -0,0 +1,61 @@
+package ru.skillbranch.devintensive.ui.main
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_archive.*
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.ui.adapters.ChatAdapter
+import ru.skillbranch.devintensive.ui.adapters.ChatItemTouchHelperCallback
+import ru.skillbranch.devintensive.ui.group.GroupActivity
+import ru.skillbranch.devintensive.viewmodels.MainViewModel
+
+class ArchiveActivity : AppCompatActivity() {
+
+    private lateinit var chatAdapter: ChatAdapter
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_archive)
+    }
+
+    private fun initViews() {
+        chatAdapter = ChatAdapter{
+            Snackbar.make(rv_archive_list, "Click on ${it.title}", Snackbar.LENGTH_LONG).show()
+        }
+        val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        val touchCallback = ChatItemTouchHelperCallback(chatAdapter){
+            val chatId = it.id
+            viewModel.addToArchive(chatId)
+            Snackbar.make(
+                rv_archive_list,
+                "Вы точно хотите добавить ${it.title} в архив?",
+                Snackbar.LENGTH_LONG
+            ).setAction(R.string.undo_string){
+                viewModel.restoreFromArchive(chatId)
+            }.show()
+        }
+        val touchHelper = ItemTouchHelper(touchCallback)
+        touchHelper.attachToRecyclerView(rv_archive_list)
+
+        with(rv_archive_list) {
+            adapter = chatAdapter
+            layoutManager = LinearLayoutManager(this@ArchiveActivity)
+            addItemDecoration(divider)
+        }
+
+
+        fab.setOnClickListener{
+            val intent = Intent(this, GroupActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+}
